9503145290942eb98223b0cd5f311a28e1371206
Hursh Desai <hursh.desai@stern.nyu.edu>
2021-03-31 16:56:02 -0700

    open in new tab

---
+++ pages/assets/js/contact.js
@@ -0,0 +1,39 @@
+document.write(`
+<section id="contact">
+	<div class="inner">
+		<section class="split">
+			<section>
+				<div class="contact-method">
+					<span class="icon solid alt fa-envelope"></span>
+					<h3>Email</h3>
+					<a href="mailto:?to=hursh.desai@stern.nyu.edu" target="_blank">hursh.desai@stern.nyu.edu</a>
+				</div>
+				<div class="contact-method">
+					<span class="icon solid alt fa-phone"></span>
+					<h3>Phone</h3>
+					<span>(669) 236-9023</span>
+				</div>
+				<div class="contact-method">
+					<span class="icon brands alt fa-linkedin-in"></span>
+					<h3>LinkedIn</h3>
+					<a href="https://www.linkedin.com/in/hursh-desai/" target="_blank">linkedin.com/in/hursh-desai</a>
+				</div>
+			</section>
+		</section>
+		<section class="split">
+			<section>
+				<div class="contact-method">
+					<span class="icon solid alt fa-briefcase"></span>
+					<h3>Upwork</h3>
+					<a href="https://www.upwork.com/o/profiles/users/~01ca3d1cdd6ebbe725/" target="_blank">upwork.com/o/hursh-desai</a>
+				</div>
+				<div class="contact-method">
+					<span class="icon brands alt fa-github"></span>
+					<h3>Github</h3>
+					<a href="https://github.com/hursh-desai" target="_blank">github.com/hursh-desai</a>
+				</div>
+			</section>
+		</section>
+	</div>
+</section>
+`);
